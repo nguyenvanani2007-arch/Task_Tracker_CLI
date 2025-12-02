96720a11b247583033e603a2e6e7993f40b9b014
nguyenvanani2007-arch <nguyenvanani2007@gmail.com>
2025-12-02 14:32:13 +0700

    feat: String to Json and add data  to file

--- TaskTracker.java
+++ TaskTracker.java
@@ -1,6 +1,8 @@
+import java.io.*;
+import java.util.*;
+import com.google.gson.Gson;
 
-import java.util.Scanner;
-
+         
 
 class Task {
     private int id;
@@ -9,34 +11,37 @@ class Task {
     private String createdAt;
     private String updatedAt;
 
-    public void getId(int id){
+    public void setId(int id){
         this.id = id;
     }
 
-    public void getDescription(String description){
+    public void setDescription(String description){
         this.description = description;
     }
 
-    public void getStatus(String status){
+    public void setStatus(String status){
         this.status = status;
     }
 
-    public void getCreatedAt(String createdAt){
+    public void setCreatedAt(String createdAt){
         this.createdAt = createdAt;
     }
 
-    public void getUpdatedAt(String updatedAt){
+    public void setUpdatedAt(String updatedAt){
         this.updatedAt = updatedAt;
     }
-
-
 }
 
+
 public class TaskTracker {
+
+    private static ArrayList<String> ListTasks = new ArrayList<>();
+    static Gson gson = new Gson();  
+    
+
     public static void main(String[] args){
-
         Scanner scanner = new Scanner(System.in);
-
+        
         while(true){
             System.out.println("1. Them task");
             System.out.println("2. Xoa tat ca cac task");
@@ -61,6 +66,26 @@ public class TaskTracker {
     public static void addTask(Scanner scanner){
         System.out.println("Mo ta todo cua ban");
         String description = scanner.nextLine();
+        Task task = new Task();
+        task.setDescription(description);
+        String json = gson.toJson(task);
+        ListTasks.add(json);
+        System.out.println(ListTasks);
+
+        writetofile(json);
+
+
+    }
+
+    public static void writetofile(String s){
+        try {
+            FileWriter filewriter = new FileWriter("Example.json");
+            filewriter.write(s);
+            filewriter.close();    
+
+        } catch (IOException e) {
+            System.out.println("Co loi");
+        }
     }
 
     public static void deleteTask(){
